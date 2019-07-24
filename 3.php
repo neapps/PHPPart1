183bdabcd09042d853a7ad74e81683be66767c65
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-24 18:06:40 +0700

    update

--- 3.php
+++ 3.php
@@ -9,24 +9,43 @@
 // 5. array
 
 //contoh tipe data "String" yang berisikan teks
-$string_satu = "teks satu";
-$sting_dua = "teks dua"
+echo "contoh tipe data String: ";
+echo "<br/>";
+$tekssatu = "teks satu";
+$teksdua = "teks dua";
+
+echo $tekssatu;
+echo "<br/>";
+echo $teksdua;
+echo "<br/>";
 
 //contoh tipe data "integer" yang berisikan bilangan asli atau bulat
+echo "contoh tipe data Integer: ";
+echo "<br/>";
 $integer_satu = 1;
 $integer_dua = 2;
 
+echo $integer_satu;
+echo "<br/>";
+echo $integer_dua;
+echo "<br/>";
+
 //contoh tipe data "Float" yang berisikan bilangan desimal
-$float_satu = 12,09;
-$float_dua = 123,456;
+//$float_satu = 12,09;
+//$float_dua = 123,456;
+
+//echo $float_satu;
+//echo "<br/>";
+//echo $float_dua;
+//echo "<br/>";
 
 //contoh tipe data "Boolean" yang berisikan value true or false 
-$boolean_satu = true;
-$boolean_dua = false;
+//$boolean_satu = true;
+//$boolean_dua = false;
 
 //contoh tipe data "Array" yang mempunyai penyimpanan banyak variabel
 $array_satu = array("DPM","BEM","SENADA");
 $array_dua = array("Braincode","DSC","NETS","MUDENG");
 
-//contoh tipe data "Object" 
+
 ?>
