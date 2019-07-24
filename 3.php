97cfe89f1dc2de3c101b80458304f0f2317075fd
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-24 17:37:10 +0700

    update

--- 3.php
+++ 3.php
@@ -7,6 +7,26 @@
 // 3. Float
 // 4. Boolean
 // 5. array
-// 6. Object
-// 7. NULL
+
+//contoh tipe data "String" yang berisikan teks
+$string_satu = "teks satu";
+$sting_dua = "teks dua"
+
+//contoh tipe data "integer" yang berisikan bilangan asli atau bulat
+$integer_satu = 1;
+$integer_dua = 2;
+
+//contoh tipe data "Float" yang berisikan bilangan desimal
+$float_satu = 12,09;
+$float_dua = 123,456;
+
+//contoh tipe data "Boolean" yang berisikan value true or false 
+$boolean_satu = true;
+$boolean_dua = false;
+
+//contoh tipe data "Array" yang mempunyai penyimpanan banyak variabel
+$array_satu = array("DPM","BEM","SENADA");
+$array_dua = array("Braincode","DSC","NETS","MUDENG");
+
+//contoh tipe data "Object" 
 ?>
