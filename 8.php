44f5d261ebbe9f5d84de439f3779d4982d4f4489
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-25 22:46:59 +0700

    Add files via upload

---
+++ 8.php
@@ -0,0 +1,15 @@
+<?php
+// ==============
+// While pada PHP
+// ==============
+// While adalah sebuah perintah untuk membuat perulangan yang tidak diketahaui berapa
+// perulangan tersebut di lakukan
+// Contoh:
+
+$x = 1;
+//x berisikan variabel angka 1
+while($x <= 10){ //disini x menampilkan variabel dari 1 kurang dari 10 = 1,2,3,4,5,6,7,8,9,10
+    echo "Angka $x <br/>"; //di panggil
+    $x++; //proses untuk menambahkan variabel yang dimulai dari angka 1
+}
+?>
