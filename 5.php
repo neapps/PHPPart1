9415b1f23f89e05269c54104c66730a63416b9eb
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-25 19:36:44 +0700

    Add files via upload

---
+++ 5.php
@@ -0,0 +1,13 @@
+<?php
+// ================
+// If else pada PHP
+// ================
+
+$kendaran = "Sepeda";
+
+if($kendaran == "Sepeda"){
+	echo "Ini kendaraan saya";
+}else{
+	echo "Ini bukan kendaraan saya";
+}
+?>
