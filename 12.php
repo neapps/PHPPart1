063de4723428720bbb37d75ca02d32e880881690
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-26 21:59:16 +0700

    ngantuk

---
+++ 12.php
@@ -0,0 +1,8 @@
+<?php
+// ============================
+// Include() dan Require() pada PHP
+// ============================
+// Include() dan Require() adalah suatu suatu fungsi pada program PHP
+// untuk menyisipkan sebuah file PHP di dalam nya.
+
+?>
