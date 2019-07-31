2d985b930eb6454dd65969525709f2fd3c6a7e0e
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-31 21:17:23 +0700

    update

---
+++ 16.php
@@ -0,0 +1,8 @@
+<!-- disini kita membuat form nya menggunakan method GET -->
+<form method = "GET" action="16.php">
+	<label>Masukan Nama</label><br/>
+	<input type="text" name="nama"><br/>
+	<label>Masukan Alamat</label><br/>
+	<input type="text" name="alamat"><br/>
+	<input type="submit" name="oke">
+</form>
