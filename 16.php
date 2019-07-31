0594995b17a727394e7c9150c05112f9218ed032
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-31 23:18:40 +0700

    Update 16.php

--- 16.php
+++ 16.php
@@ -1,4 +1,14 @@
+<!-- 
+===========================================
+Membuat Form dengan method GET dan POST PHP
+===========================================
+pertama kita akan membuat file lagi dengan judul
+index.php, karena di dalam index.php ini kita
+akan menampilkan hasil Formnya
+berikut adalah contoh nya -->
+
 <!-- disini kita membuat form nya menggunakan method GET -->
+
 <form method = "GET" action="16.php">
 	<label>Masukan Nama</label><br/>
 	<input type="text" name="nama"><br/>
