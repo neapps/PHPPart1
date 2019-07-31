8631d897202e0ef93df912198b775cee0d87900f
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-07-31 23:19:25 +0700

    remake

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-// ===========================================
-// Membuat Form dengan method GET dan POST PHP
-// ===========================================
-// pertama kita akan membuat file lagi dengan judul
-// index.php, karena di dalam index.php ini kita
-// akan menampilkan hasil Formnya
-// berikut adalah contoh nya
-
 // membuat method dan menangkap method nama
 $nama = $_GET['nama'];
 $alamat = $_GET['alamat'];
