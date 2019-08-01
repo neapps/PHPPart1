16eee14d0429cfb154dd27ffb9a4388dc1f142c4
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-08-01 20:01:51 +0700

    remake

--- index.php
+++ index.php
@@ -9,11 +9,19 @@
 </form>
 
 <!--disini kita membuat form nya menggunakan method POST 
-<form method = "GET" action="index.php">
+<form method = "POST" action="index.php">
 	<label>Masukan Nama</label><br/>
 	<input type="text" name="nama"><br/>
 	<label>Masukan Alamat</label><br/>
 	<input type="text" name="alamat"><br/>
 	<input type="submit" name="oke">
-</form>
--->
+</form> -->
+
+<!-- disini kita membuat form nya menggunakan method REQUEST
+<form method = "POST" action="index.php">
+	<label>Masukan Nama</label><br/>
+	<input type="text" name="nama"><br/>
+	<label>Masukan Alamat</label><br/>
+	<input type="text" name="alamat"><br/>
+	<input type="submit" name="oke">
+</form> -->
